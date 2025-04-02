400e317a631b97fe8b84ba537d438e04709ba891
serwmwser <63735404+serwmwser@users.noreply.github.com>
2025-04-02 06:03:37 +0300

    Update init-blockchain.sh

--- init-blockchain.sh
+++ init-blockchain.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-cd polygon-edge
+POLYGON_BIN="/workspaces/ZEROCHAIN/polygon-edge/polygon-edge"
+GENESIS_FILE="/workspaces/ZEROCHAIN/polygon-edge/genesis.json"
 
-./polygon-edge genesis --consensus ibft \
+"$POLYGON_BIN" genesis --consensus ibft \
   --block-gas-limit 10000000 \
   --epoch-size 100000 \
   --premine 0xc2e5650f84Eeb9e4011afbb398108ea302cB17A6:1000000000000000000000 \
   --chain-id 12345 \
   --name "MyTokenChain" \
-  --dir ./genesis.json
-
-cd ..
+  --dir "$GENESIS_FILE"
